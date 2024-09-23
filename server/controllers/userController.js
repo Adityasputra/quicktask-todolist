88b89a7a3a3a82919820278b3e6c30a69de8b7aa
Adityasputra <aditsputra.cv@gmail.com>
2024-09-24 01:09:41 +0700

    Feat: create a Controller for new User

---
+++ server/controllers/userController.js
@@ -0,0 +1,34 @@
+const { User } = require("../models");
+
+module.exports = class UserController {
+  static async register(req, res) {
+    try {
+      const { username, email, password } = req.body;
+      const newUser = await User.create({
+        username,
+        email,
+        password,
+      });
+
+      res.status(201).json({
+        id: newUser.id,
+        username: newUser.username,
+        email: newUser.email,
+      });
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const validate = error.errors.map((er) => er.message);
+        res.status(400).json({
+          message: validate,
+        });
+      } else {
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
+    }
+  }
+};
