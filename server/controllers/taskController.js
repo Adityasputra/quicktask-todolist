a7f9d58141cb46fb1444ccce1c8ea8e6fe03d8ec
Adityasputra <aditsputra.cv@gmail.com>
2024-09-25 00:32:01 +0700

    Feat: create a API for update Todolist

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -5,7 +5,6 @@ module.exports = class TaskController {
     try {
       const { title, description } = req.body;
       const { id } = req.user;
-      console.log(req.user, "<<< This req user");
       const newTask = await Task.create({
         title,
         description,
@@ -14,6 +13,59 @@ module.exports = class TaskController {
       });
 
       res.status(201).json(newTask);
+    } catch (error) {
+      console.log(error);
+      if (error.name === "SequelizeValidationError") {
+        const validate = error.errors.map((er) => er.message);
+        res.status(400).json({
+          message: validate,
+        });
+      } else {
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
+    }
+  }
+
+  static async getAllTask(req, res) {
+    try {
+      const tasks = await Task.findAll();
+      res.status(200).json(tasks);
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  }
+
+  static async updateTask(req, res) {
+    try {
+      const { title, description, status } = req.body;
+      const { id } = req.params;
+      const updateTask = await Task.findByPk(id);
+      if (!updateTask) {
+        return res.status(404).json({
+          message: "Task Not Found",
+        });
+      } else {
+        await Task.update(
+          {
+            title,
+            description,
+            status,
+          },
+          {
+            where: {
+              id,
+            },
+          }
+        );
+
+        res.status(200).json({
+          message: "Successfully update the Task",
+        });
+      }
     } catch (error) {
       console.log(error);
       res.status(500).json({
--- server/routes/task.js
+++ server/routes/task.js
@@ -1,8 +1,14 @@
-const { createTask } = require("../controllers/taskController");
+const {
+  createTask,
+  getAllTask,
+  updateTask,
+} = require("../controllers/taskController");
 const { auth } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
 router.post("/", auth, createTask);
+router.get("/", auth, getAllTask);
+router.put("/:id/update", updateTask);
 
 module.exports = router;
