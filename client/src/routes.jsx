185d97a5c4ed21cf6a978b0cc5c087b152cbf416
Adityasputra <aditsputra.cv@gmail.com>
2024-09-27 00:21:56 +0700

    Feat: setup router for client

---
+++ client/src/routes.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <h1> Hello This Login</h1>,
+    loader: () => {
+      if (localStorage.access_token) return redirect("/");
+      return null;
+    },
+  },
+  {
+    path: "/register",
+    element: <h1> Hello This Register</h1>,
+    loader: () => {
+      if (localStorage.access_token) return redirect("/");
+      return null;
+    },
+  },
+  {
+    path: "/",
+    element: <h1>Hello This Home</h1>,
+    loader: () => {
+      if (!localStorage.access_token) return redirect("/login");
+      return null;
+    },
+    children: [
+      {
+        path: "",
+        element: <h1>Hello this Home</h1>,
+      },
+    ],
+  },
+]);
+
+export default router;
