7375e8691a0fe7d3ce47dc1462b0b2409faf827f
Adityasputra <aditsputra.cv@gmail.com>
2024-09-27 17:47:34 +0700

    Feat: add a login from Google

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import axios from "../services/apiService";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
@@ -26,6 +26,38 @@ export default function LoginPage() {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    async function handleCredentialResponse(response) {
+      try {
+        const { data } = await axios({
+          method: "POST",
+          url: "api/users/google-login",
+          headers: {
+            google_token: response.credential,
+          },
+        });
+
+        localStorage.setItem("access_token", data.access_token);
+        // console.log(data, "This data");
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    google.accounts.id.initialize({
+      client_id: import.meta.env.VITE_CLIENT_ID,
+      callback: handleCredentialResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("buttonGoogle"), {
+      theme: "outline",
+      size: "large",
+    });
+
+    google.accounts.id.prompt();
+  }, []);
 
   return (
     <>
@@ -61,6 +93,7 @@ export default function LoginPage() {
                 <input
                   className="bg-gray-200 text-[#2f3640] rounded py-2 px-4 block w-full appearance-none focus:outline-none focus:shadow-md"
                   type="email"
+                  value={email}
                   placeholder="Enter your email"
                   onChange={(e) => setEmail(e.target.value)}
                 />
@@ -72,6 +105,7 @@ export default function LoginPage() {
                 <input
                   className="bg-gray-200 text-[#2f3640] rounded py-2 px-4 block w-full appearance-none focus:outline-none focus:shadow-md"
                   type="password"
+                  value={password}
                   placeholder="Enter your password"
                   onChange={(e) => setPassword(e.target.value)}
                 />
@@ -90,34 +124,10 @@ export default function LoginPage() {
                 <span className="border-b w-1/5 lg:w-1/4"></span>
               </div>
 
-              <a
-                href="#"
-                className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100"
-              >
-                <div className="px-4 py-3">
-                  <svg class="h-6 w-6" viewBox="0 0 40 40">
-                    <path
-                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                      fill="#FFC107"
-                    />
-                    <path
-                      d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-                      fill="#FF3D00"
-                    />
-                    <path
-                      d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-                      fill="#4CAF50"
-                    />
-                    <path
-                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                      fill="#1976D2"
-                    />
-                  </svg>
-                </div>
-                <h1 className="px-4 py-3 w-5/6 text-center text-[#2f3640] font-bold">
-                  Sign in with Google
-                </h1>
-              </a>
+              <div className="flex justify-center mt-4">
+                <div id="buttonGoogle"></div>
+              </div>
+
               <div className="mt-4 text-center">
                 <p className="text-sm text-gray-600">
                   Don't have an account?{" "}
