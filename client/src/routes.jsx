6ad15a18b160a9e9f38bb57506c5953da94068d5
Adityasputra <aditsputra.cv@gmail.com>
2024-09-27 13:45:43 +0700

    Feat: create a Login page

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
   {
     path: "/login",
-    element: <h1> Hello This Login</h1>,
+    element: <LoginPage />,
     loader: () => {
       if (localStorage.access_token) return redirect("/");
       return null;
